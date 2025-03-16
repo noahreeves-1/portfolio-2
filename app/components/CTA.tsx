e00b061c7667d5a6c3c9f59286c100a37e0dea6a
Noah Kim <nhkreeves@gmail.com>
2025-03-15 20:43:54 -0700

    updated icons

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import Preparation from "@/public/preparation.svg";
 import { motion } from "framer-motion";
 import Brush from "@/public/brush.svg";
@@ -270,8 +269,8 @@ const CTA = () => {
                       y={70 - 12}
                     >
                       <div className="w-6 h-6">
-                        <Image
-                          src={step.svgComponent}
+                        <img
+                          src={step.svgComponent.src}
                           alt={step.name}
                           width={24}
                           height={24}
@@ -411,29 +410,16 @@ const CTA = () => {
                   variants={iconAnimation(index)}
                 >
                   {step.useSvgFile ? (
-                    // For SVG files, use a different approach for mobile
-                    <g transform={`translate(48, ${cy - 12})`}>
-                      {step.name === "Design" && (
-                        <path
-                          d="M13 2H5.5C4.12 2 3 3.12 3 4.5V19.5C3 20.88 4.12 22 5.5 22H18.5C19.88 22 21 20.88 21 19.5V9M13 2L21 9M13 2V9H21M14 14.5C14 15.33 13.33 16 12.5 16C11.67 16 11 15.33 11 14.5C11 13.67 11.67 13 12.5 13C13.33 13 14 13.67 14 14.5Z"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          fill="none"
+                    <foreignObject width="24" height="24" x={48} y={cy - 12}>
+                      <div className="w-6 h-6">
+                        <img
+                          src={step.svgComponent.src}
+                          alt={step.name}
+                          width={24}
+                          height={24}
                         />
-                      )}
-                      {step.name === "Implement" && (
-                        <path
-                          d="M16 18L22 12L16 6M8 6L2 12L8 18"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          fill="none"
-                        />
-                      )}
-                    </g>
+                      </div>
+                    </foreignObject>
                   ) : (
                     <path
                       d={step.icon}
@@ -510,8 +496,8 @@ const CTA = () => {
             viewport={{ once: true, amount: 0.3 }}
             variants={imageFadeIn}
           >
-            <Image
-              src={Preparation}
+            <img
+              src={Preparation.src}
               alt="Preparation"
               width={500}
               height={500}
